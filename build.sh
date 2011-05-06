63f9389cf768ff817806d3c9695181ecac0fc914
BROUILLARD Cyril <cyril.brouillard@gmail.com>
2011-05-07 00:57:43 +0200

    script dayly_build

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+VERSION="0-02"
+echo "### Nettoyage"
 make clean
 rm *.elf *.gba *.s
+echo "### Compilation"
 make
-gvba FuriousAdvanceTracker_v0-02.gba
+echo "### Copie dans les dayly_builds"
+cp FuriousAdvanceTracker_v$VERSION.gba dayly_build/FuriousAdvanceTracker_v$VERSION-`date '+%d-%B-%Y'`.gba
+echo "### Lancement de l'émulateur"
+gvba FuriousAdvanceTracker_v$VERSION.gba
