bf989daa870ecef80b7ff179a7fb272f7f54a44e
BROUILLARD Cyril <cyril.brouillard@gmail.com>
2011-05-30 15:14:27 +0200

    configuration de GBFS OK

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ make clean
 rm *.elf *.gba *.s
 echo "### Compilation"
 make
+echo "### Padbin pour execution du GBFS"
+wine padbin.exe 256 FuriousAdvanceTracker_v$VERSION.gba
+echo "### Generation du SAMPLE filesystem"
+wine gbfs.exe default_samples.gbfs  samples/*.wav
+echo "### Patch du GBA pour inclusion des samples"
+cat FuriousAdvanceTracker_v$VERSION.gba default_samples.gbfs > TMP.gba
+mv TMP.gba FuriousAdvanceTracker_v$VERSION.gba
 echo "### Copie dans les daily_builds"
 cp FuriousAdvanceTracker_v$VERSION.gba daily_builds/FuriousAdvanceTracker_v$VERSION-`date '+%d-%B-%Y'`.gba
 echo "### Lancement de l'émulateur"
