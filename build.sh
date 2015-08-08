06f50c3d8f40ecbb38189c7713ac7711344bcc07
Cyril Brouillard <cbrouillard@mirakl.com>
2015-08-08 23:57:51 +0200

    Nouveaux samples mauvais encodage pour le moment.

--- build.sh
+++ build.sh
@@ -18,13 +18,14 @@ wine padbin.exe 256 FAT_v$VERSION.gba
 
 echo "### Generation du SAMPLE filesystem"
 rm *.gbfs
-    #objcopy -v -I binary -O elf32-little --rename-section .data=.rodata in_file out_file
+#objcopy -v -I binary -O elf32-little --rename-section .data=.rodata in_file out_file
 wine gbfs.exe infos.gbfs samples/0nbkit
 wine gbfs.exe default_samples.gbfs samples/0infos samples/*.snd
+wine gbfs.exe tr606_samples.gbfs samples/tr-606-7/0infos samples/tr-606-7/*.snd
 wine gbfs.exe battery_samples.gbfs samples/battery/0infos samples/battery/*.raw
 
 echo "### Patch du GBA pour inclusion des samples"
-cat FAT_v$VERSION.gba infos.gbfs default_samples.gbfs battery_samples.gbfs > TMP.gba
+cat FAT_v$VERSION.gba infos.gbfs default_samples.gbfs tr606_samples.gbfs battery_samples.gbfs > TMP.gba
 mv TMP.gba FAT_v$VERSION.gba
 
 echo "### Copie dans les daily_builds"
