04f79ec880bfa5dcf7a5dce3115e938446ddf52c
Cyril Brouillard <cbrouillard@mirakl.com>
2015-08-09 15:39:56 +0200

    Petite amélioration du script de conversion des samples en kit

--- samples/buildSamples.sh
+++ samples/buildSamples.sh
@@ -1,3 +1,4 @@
-echo "7" > 0nbkit
+nbKit=$(expr `ls -l | wc -l` - 3)
+echo "$nbKit" > 0nbkit
 find . -name "*.wav" -exec ffmpeg -y -i {} -ac 1 -ar 16000 -f s8 -acodec pcm_s8 {}.snd \;
 find . -name "*.mp3" -exec ffmpeg -y -i {} -ac 1 -ar 16000 -f s8 -acodec pcm_s8 {}.snd \;
