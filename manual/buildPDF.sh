bf490f72dcb28f922768429bc387cac35135bf93
Cyril Brouillard <cyril.brouillard@eileo.com>
2015-08-26 17:35:42 +0200

    Gros travail sur la doc FR et début de la traduction pour la doc EN.

--- manual/buildPDF.sh
+++ manual/buildPDF.sh
@@ -19,3 +19,14 @@ latex2html -split 5 -iso_language 'FR' $FILENAME.iso.tex
 rm $FILENAME.iso.tex
 cp FAT_documentation_FR.iso.css FAT_documentation_FR.iso/
 echo "**** Terminé ****"
+
+FILENAME=FAT_documentation_EN
+PDF_FILE_TO_GENERATE=FAT_documentation_EN.pdf
+
+echo "**** Construction du PDF ****"
+rm ./$PDF_FILE_TO_GENERATE
+pdflatex -output-directory=$DIR_TMP_BUILD  $FILENAME.tex
+pdflatex -output-directory=$DIR_TMP_BUILD  $FILENAME.tex
+mv $DIR_TMP_BUILD/$FILENAME.pdf ./$PDF_FILE_TO_GENERATE
+echo "**** Terminé ****"
+
